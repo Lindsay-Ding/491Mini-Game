6f624f4155d36e66fa6d7d7619241a0d9e82392d
Lindsay Ding <lindingdlx@gmail.com>
2023-01-29 15:51:32 -0800

    First commit

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const gameEngine = new GameEngine();
+const newRow = new NewRow();
 
 const ASSET_MANAGER = new AssetManager();
 
@@ -6,7 +7,11 @@ ASSET_MANAGER.downloadAll(() => {
 	const canvas = document.getElementById("gameWorld");
 	const ctx = canvas.getContext("2d");
 
-	gameEngine.init(ctx);
+	// gameEngine.init(ctx);
 
-	gameEngine.start();
+	// gameEngine.start();
+
+	newRow.init(ctx);
+	newRow.draw(ctx);
+	
 });
